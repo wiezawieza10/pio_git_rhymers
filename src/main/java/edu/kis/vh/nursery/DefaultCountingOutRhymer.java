9ddcf244b1b6c03aef37e81c9e4bea96487d4ca0
wiezawieza10 <wiezaawieza10@op.pl>
2021-01-19 21:48:15 +0100

    poprawa formatowania kodu

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -3,39 +3,39 @@ package edu.kis.vh.nursery;
 public class DefaultCountingOutRhymer {
 
 
-	public static final int INITIAL = -1;
-	public static final int LENGTH = 12;
-	public static final int RETURN = -1;
+    public static final int INITIAL = -1;
+    public static final int LENGTH = 12;
+    public static final int RETURN = -1;
 
 
-	private final int[] NUMBERS = new int[CAPACITY];
+    private final int[] NUMBERS = new int[LENGTH];
 
 
-	public int total = INITIAL;
+    public int total = INITIAL;
 
-	public void countIn(int in) {
-		if (!isFull())
-			NUMBERS[++total] = in;
-	}
+    public void countIn(int in) {
+        if (!isFull())
+            NUMBERS[++total] = in;
+    }
 
-		public boolean callCheck() {
-			return total == INITIAL;
-		}
-		
-			public boolean isFull() {
-				return total == LENGTH-1;
-			}
-		
-				protected int peekaboo() {
-					if (callCheck())
-						return RETURN;
-					return NUMBERS[total];
-				}
-			
-					public int countOut() {
-						if (callCheck())
-							return RETURN;
-						return NUMBERS[total--];
-					}
+    public boolean callCheck() {
+        return total == INITIAL;
+    }
+
+    public boolean isFull() {
+        return total == LENGTH - 1;
+    }
+
+    protected int peekaboo() {
+        if (callCheck())
+            return RETURN;
+        return NUMBERS[total];
+    }
+
+    public int countOut() {
+        if (callCheck())
+            return RETURN;
+        return NUMBERS[total--];
+    }
 
 }
--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -3,41 +3,41 @@ package edu.kis.vh.nursery.list;
 public class IntLinkedList {
 
 
-	public static final int RETURN = -1;
+    public static final int RETURN = -1;
 
-	Node last;
-	int i;
+    Node last;
+    int i;
 
-	public void push(int i) {
-		if (last == null)
-			last = new Node(i);
-		else {
-			last.next = new Node(i);
-			last.next.prev = last;
-			last = last.next;
-		}
-	}
+    public void push(int i) {
+        if (last == null)
+            last = new Node(i);
+        else {
+            last.next = new Node(i);
+            last.next.prev = last;
+            last = last.next;
+        }
+    }
 
-	public boolean isEmpty() {
-		return last == null;
-	}
+    public boolean isEmpty() {
+        return last == null;
+    }
 
-	public boolean isFull() {
-		return false;
-	}
+    public boolean isFull() {
+        return false;
+    }
 
-	public int top() {
-		if (isEmpty())
-			return RETURN;
-		return last.value;
-	}
+    public int top() {
+        if (isEmpty())
+            return RETURN;
+        return last.value;
+    }
 
-	public int pop() {
-		if (isEmpty())
-			return RETURN;
-		int ret = last.value;
-		last = last.prev;
-		return ret;
-	}
+    public int pop() {
+        if (isEmpty())
+            return RETURN;
+        int ret = last.value;
+        last = last.prev;
+        return ret;
+    }
 
 }
